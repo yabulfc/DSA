e33277c92776e993979b7294a6a678ac59cec013
Yeabsira Yeneneh <yabuyeneneh10@gmail.com>
2026-03-07 17:32:07 +0300

    did some example code for updating an array

---
+++ UpdateArray.py
@@ -0,0 +1,11 @@
+def update(num , index):  # a function that take a number  and update/replace it to a given index
+
+        numbers = [0, 1, 2, 3, 4]
+
+        print('The Element at index', index , 'is: ' , numbers[index])
+        numbers[index] = num
+
+        for num in numbers:
+                print(num)
+
+update(100 , 2)  
