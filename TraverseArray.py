0886db4d648ff40dc8fcfbf1cf9fcf0daef4d902
Yeabsira Yeneneh <yabuyeneneh10@gmail.com>
2026-03-07 16:45:53 +0300

    wrote an example code for traversing

---
+++ TraverseArray.py
@@ -0,0 +1,14 @@
+def traversing(numbers , strings):
+    
+    for num in numbers:
+        print(num)
+    
+    for word in strings:
+        print(word)
+
+
+
+traversing([1,2,3] , ['yeab','abebe', 'girma'])
+
+
+    
