b0302ad043232c6fb021341c0c5df5423ca8df2c
Yeabsira Yeneneh <yabuyeneneh10@gmail.com>
2026-06-19 11:20:07 +0300

    did a heterogenous element example

---
+++ 10HeterogeneousElements.py
@@ -0,0 +1,11 @@
+data = [None] * 5 # Create a list of size 5 to hold heterogeneous elements
+data[0] = 10    
+data[1] = "Hello"
+data[2] = 3.14
+data[3] = True
+data[4] = 'A'
+
+print("Heterogeneous elements in the list:" , data) # Output: [10, 'Hello', 3.14, True, 'A']
+
+for x in data:
+    print(x) # Output: 10, Hello, 3.14, True, A (each on a new line)--
