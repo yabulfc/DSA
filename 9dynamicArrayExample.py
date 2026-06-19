4ff380c55490c12e07f4d79f8543e000479b04bd
Yeabsira Yeneneh <yabuyeneneh10@gmail.com>
2026-06-19 10:58:08 +0300

    did some examples about dynamic array

--- 9dynamicArrayExample.py
+++ 9dynamicArrayExample.py
@@ -4,17 +4,27 @@ fruits.append("Apple")
 fruits.append("Banana")
 fruits.append("Cherry")
 
-print("friuts list:" + fruits) # Output: ['Apple', 'Banana', 'Cherry']   
+print("friuts list:" ,fruits) # Output: ['Apple', 'Banana', 'Cherry']   
 
-friuts = friuts.get(1) # Get the second element (index 1 is "banana")
-print("Second fruit:" + fruits) # Output: Banana
+secFriuts = fruits[1] # Get the second element (index 1 is "banana")
+print("Second fruit:" , secFriuts) # Output: Banana
 
 containsMango = "Mango" in fruits # Check if "Mango" is in the list and returns in boolean
-print("Contains Mango:" + containsMango) # Output: False
+print("Contains Mango:" , containsMango) # Output: False
 
-removeFruit = fruits.remove("Banana") # Remove "Banana" from the list
-print('removed fruit:' + removeFruit) # Output: Banana
+removeFruit = "Banana"
+removedFruit = fruits.remove("Banana") # Remove "Banana" from the list
+print('removed fruit:' ,removeFruit ) # Output: Banana
 
-print("Updated fruits list:" + fruits) # Output: ['Apple', 'Cherry']
+print("Updated fruits list:" , fruits) # Output: ['Apple', 'Cherry']
 
 isEmpty = len(fruits) == 0 # Check if the list is empty and returns in boolean
+print("Is fruits list empty:" , isEmpty) # Output: False
+
+size = len(fruits) # Get the size of the list
+print("Size of the fruits list:" , size) # Output 2
+
+fruits = fruits.clear() # Clear the list
+isEmptyAfterClear = not fruits # Check if the list is empty after clear and returns in boolean
+print("Is fruits list empty after clear:" , isEmptyAfterClear) # Output True
+
