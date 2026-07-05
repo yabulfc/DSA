cca9e14a12d0840ae4ad54e64390e1f7afcbb9b1
Yeabsira Yeneneh <yabuyeneneh10@gmail.com>
2026-07-05 17:22:08 +0300

    leetcode problem 2 solved

---
+++ 14leetcodeProblem.py
@@ -0,0 +1,20 @@
+# # Leetcode question number 121
+
+# # Best timee to buy and Sell Stock 
+
+
+
+# answer
+
+def maxProfit(prices):
+    min_price = float('inf') #Set min_price to infinity
+    max_profit = 0
+
+    for price in prices:
+        if price < min_price:
+            min_price = price
+        elif price - min_price > max_profit:
+            max_profit = price - min_price
+    return max_profit
+
+print(maxProfit([7,1,5,3,6,4]))
